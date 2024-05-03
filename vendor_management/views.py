e968f8086f5c4ad5cf19c1be9d560a37ea7ffac6
Rutvik Kapse <kapse.rutvik04@gmail.com>
2024-05-04 03:06:47 +0530

    Created Vendor viewset.

--- vendor_management/views.py
+++ vendor_management/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Vendor
+from .serializers import VendorSerializer
+
 
 # Create your views here.
+class VendorViewSet(ModelViewSet):
+    http_method_names = ['get', 'put', 'post', 'delete']
+
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
