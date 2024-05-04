7ccb24967f7806eb2bfccccf2692b90c96e45846
Rutvik Kapse <kapse.rutvik04@gmail.com>
2024-05-04 16:18:12 +0530

    Overridden retrieve method.
    Vendor code will be used to retrieve vendors instead of id.

--- vendor_management/views.py
+++ vendor_management/views.py
@@ -1,6 +1,8 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from .models import Vendor
-from .serializers import VendorSerializer
+from rest_framework.response import Response
+from .models import Vendor, PurchaseOrder
+from .serializers import VendorSerializer, PurchaseOrderSerializer
 
 
 # Create your views here.
@@ -9,3 +11,17 @@ class VendorViewSet(ModelViewSet):
 
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        # Remove the default behavior of retrieving by pk
+        instance = None
+        # Assume product_code is used instead of pk
+        vendor_code = kwargs.get('pk')
+        print(kwargs)
+        if vendor_code is not None:
+            queryset = self.filter_queryset(self.get_queryset())
+            instance = queryset.filter(vendor_code=vendor_code).first()
+            if instance is not None:
+                serializer = self.get_serializer(instance)
+                return Response(serializer.data)
+        return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
